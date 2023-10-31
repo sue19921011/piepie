dae26fbe2b0b77768aba7d46d3d3277f97ceb030
piepie <sue19921011@gmail.com>
2023-10-31 16:12:58 +0800

    1031

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <script setup lang="ts">
-import English from "./components/EnglishDB.vue"; 
+import English from "./components/EnglishDB.vue";
 import Chemistry from "./components/Chemistry.vue";
-import ChemistryCheckbox from "./components/ChemistryCheckbox.vue";
-import { reactive } from "vue";
+import ChemistryRadio from "./components/ChemistryRadio.vue";
+import { reactive,ref } from "vue";
+let drawer = ref(false);
 let choice = reactive({title:'英文', value:"English"});
 let items=[
   {title:'英文', value:"English"},
   {title:'化學', value:"Chemistry"},
-  {title:'化學單選', value:"ChemistryCheckbox"}]
+  {title:'化學單選', value:"ChemistryRadio"}]
 </script>
 
 <template>
-  <div>
-    <v-select label="請選擇" v-model="choice.value" :items="items" item-title="title" item-value="value">
-    </v-select>
-    <Suspense>
-      <English v-if="choice.value === 'English'" />
-      <Chemistry v-else-if="choice.value === 'Chemistry'" />
-      <ChemistryCheckbox v-else-if="choice.value === 'ChemistryCheckbox'" />
-    </Suspense>
-  </div>
+  <v-app class="rounded rounded-md">
+    <v-app-bar>
+      <v-icon icon="plus"></v-icon>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer">menu</v-app-bar-nav-icon>
+        <v-app-bar-title>Application bar</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer floating permanent v-model="drawer">
+      <v-list>
+        <v-list-item v-for="item in items"
+          :title="item.title" 
+          :key="item.value" 
+          :active="item.value === choice.value"
+          @click="choice.value = item.value">
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex flex-column align-start justify-start mb-6" style="min-height: 300px;">
+      <Suspense>
+        <English v-if="choice.value === 'English'" />
+        <Chemistry v-else-if="choice.value === 'Chemistry'"/>
+        <ChemistryRadio v-else />
+      </Suspense>
+    </v-main>
+  </v-app>
 </template>
--- src/components/EnglishDB.vue
+++ src/components/EnglishDB.vue
@@ -1,42 +1,51 @@
 <script setup lang="ts">
-import { reactive } from "vue";
-import { collection, getDocs, getFirestore } from "firebase/firestore"; 
+import { reactive, watch } from "vue";
+import { collection, getDocs, getFirestore, query, where } from "firebase/firestore"; 
 import app from "@/components/settings/FirebaseConfig.vue"
+
+const state = reactive({choice:1 , answer:[''],message: [''],exams:[{question:"", answer:""}] });
+
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-const querySnapshot = await getDocs(collection(db, "english"));
-// const data = reactive<{question:string, answer:string}[]>([]);
-let exams:{question:string, answer:string}[]=[];
-querySnapshot.forEach((doc) => {
-  exams.push({question:doc.data().question,answer:doc.data().answer});
-  console.log(`${doc.id} => ${doc.data()}`);
-});
-let answer = "";
-const state = reactive({ message: "", currentQuestion: 0 });
-// let exams = [
-//   { question: "氫的化學符號", answer: "H" },
-//   { question: "氧的化學符號", answer: "O" },
-//   { question: "氦的化學符號", answer: "He" },
-// ];
-function generateQuestion() {
-  if (exams[state.currentQuestion].answer === answer) {
-    state.message = "答案正確";
-    if (state.currentQuestion + 1 < exams.length) {
-      state.currentQuestion++;
+const examCollection = collection(db, "english");
+generateQuestions();
+let units=[
+  {title:'單元一', value:1},
+  {title:'單元二', value:2},
+]
+//點選select時，會改變state.choice，利用watch，當state.choice改變時，重新讀取題目
+watch(() => state.choice,generateQuestions);
+async function generateQuestions(){
+  console.log(state.choice)
+  state.exams=[];
+  const queryExam = query(examCollection, where("unit", "==", state.choice));
+  const querySnapshot = await getDocs(queryExam);
+  querySnapshot.forEach((doc) => {
+    state.exams.push({question:doc.data().question,answer:doc.data().answer});
+  });
+}
+
+function checkAnswers() {
+  state.message = []; // clear previous messages
+  for (let i in state.exams) {
+    if (state.answer[i] !== state.exams[i].answer) {
+      state.message[i]="不正確";
     }
-  } else {
-    state.message = "答案錯誤";
+    else {
+      state.message[i]="正確";}
   }
 
 }
-generateQuestion();
 </script>
 <template>
-  <div>
-    <v-text-field v-model="answer" 
-      :label="exams[state.currentQuestion].question" 
-      :messages="state.message">
-    </v-text-field>
-    <v-btn color="primary" @click="generateQuestion">下一題</v-btn>
-  </div>
+<v-container>
+  <v-select label="請選擇" v-model="state.choice" :items="units" >
+  </v-select>
+  <v-card v-for="(exam, index) in state.exams" :key="index">
+    <v-text-field v-model="state.answer[index]" 
+    :label="exam.question" 
+    :messages="state.message[index]"></v-text-field>
+  </v-card>
+  <v-btn color="primary" @click="checkAnswers">檢查答案</v-btn>
+</v-container>
 </template>
